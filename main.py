f49b6e4e04f1ef2175ec51090740754f6c36053f
JosefAf <126606751+JosefAf@users.noreply.github.com>
2025-05-19 13:37:44 +0000

    Running both snakes from same replit app

--- main.py
+++ main.py
@@ -10,24 +10,53 @@
 # To get you started we've included code to prevent your Battlesnake from moving backwards.
 # For more info see docs.battlesnake.com
 
+# IMPORTANT INFO: When creating the battlesnakes, grab the server URL and add "snakeA" to the end of the URL when creating Snake A, such that the URL ends with "/snakeA". Similar for Snake D, the URL should end with "/snakeD". 
+
 import typing
 from SnakeAI import SnakeAI
+from flask import Flask, request, jsonify #For collaboration between snakes
 
-snake_ai = SnakeAI() # Create an instance of the SnakeAI class
+snake_a_ai = SnakeAI() # Create an instance of the SnakeAI class for Snake A
+snake_d_ai = SnakeAI() # Create an instance of the SnakeAI class for Snake D
+
+app = Flask(__name__) #Start web server in this file
+shared_data = {}  # Shared data in dictionary for coordination between snakes
+
+#Just testing the server
+@app.route("/")
+def server_root():
+    return "Server is running!"
 
 # info is called when you create your Battlesnake on play.battlesnake.com
 # and controls your Battlesnake's appearance
 # TIP: If you open your Battlesnake URL in a browser you should see this data
-def info() -> typing.Dict:
+@app.route("/snakeA", methods=["GET"])
+def info_snakeA() -> typing.Dict:
     print("INFO")
-
     return {
         "apiversion":
         "1",
         "Josepi The Moustache & Ram The Ham":
-        "Johnny SilverSnake",
+        "Johnny Aggro",
         "color":
-        "#c0d5c2",  
+        "#f20f29",  
+        "head":
+        "dead",
+        "tail":
+        "freckled",
+        "version":
+        "1"
+    }
+@app.route("/snakeD", methods=["GET"])
+def info_snakeD() -> typing.Dict:
+    print("INFO")
+    return {
+        "apiversion":
+        "1",
+        "Josepi The Moustache & Ram The Ham":
+        "Johnny Defendo",
+        "color":
+        "#2417bd",  
         "head":
         "dead",
         "tail":
@@ -36,15 +65,28 @@ def info() -> typing.Dict:
         "1"
     }
 
-
 # start is called when your Battlesnake begins a game
-def start(game_state: typing.Dict):
+@app.route("/snakeA/start", methods=["POST"])
+def start_snakeA():
+    #Retrieve game_state like this when using Flask:
+    game_state = request.get_json() #Not used atm
     print("Start Game:")
-    snake_ai.reset()
-
+    snake_a_ai.reset
+    return {"status": "ok"} #Flask gets upset if this line is omitted
+@app.route("/snakeD/start", methods=["POST"])
+def start_snakeD():
+    #Retrieve game_state like this when using Flask:
+    game_state = request.get_json() #Not used atm
+    print("Start Game:")
+    snake_d_ai.reset()
+    return {"status": "ok"}
 
 # end is called when your Battlesnake finishes a game
-def end(game_state: typing.Dict):
+@app.route("/snakeA/end", methods=["POST"])
+@app.route("/snakeD/end", methods=["POST"])
+def end():
+    #Retrieve game_state like this when using Flask:
+    game_state = request.get_json() #Not used atm
     print("End Game.\n")
 
 
@@ -67,18 +109,33 @@ see the details in the docs - Game object: https://docs.battlesnake.com/api/obje
 Example of Move Request (the game_state received by move()) and Move Response (move()'s return value, new move dict):                 
 https://docs.battlesnake.com/api/example-move"""
 
-def move(game_state: typing.Dict) -> typing.Dict:
+@app.route("/snakeA/move", methods=["POST"])
+def move_snakeA():
+    #Retrieve game_state like this when using Flask:
+    game_state = request.get_json()
+    
     # Snake AI
-    snake_ai.update_state(game_state)
+    snake_a_ai.update_state(game_state)
 
-    next_move = snake_ai.get_Next_Move()
+    next_move = snake_a_ai.get_Next_Move()
     
     print(f"MOVE {game_state['turn']}: {next_move}")
-    return {"move": next_move, "shout": "I'm a snake! Hissss!"}
+    return {"move": next_move, "shout": "I'm snake A! Hissss!"}
+
+@app.route("/snakeD/move", methods=["POST"])
+def move_snakeD():
+    #Retrieve game_state like this when using Flask:
+    game_state = request.get_json()
+    
+    # Snake AI
+    snake_d_ai.update_state(game_state)
+
+    next_move = snake_d_ai.get_Next_Move()
+
+    print(f"MOVE {game_state['turn']}: {next_move}")
+    return {"move": next_move, "shout": "I'm snake D! Hissss!"}
 
 # Start server when `python main.py` is run
 if __name__ == "__main__":
-    from server import run_server
-
-    run_server({"info": info, "start": start, "move": move, "end": end})
+    app.run(host="0.0.0.0", port=8080)
 
